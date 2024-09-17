cb9fce7c550a276f74bfac71dec1114820e73c8f
Deivi Ronald Veliz Camac <dveliz-c@c2r17s4.42madrid.com>
2024-09-17 20:06:08 +0200

    header

--- PROC/funciones/ft_memset.c
+++ PROC/funciones/ft_memset.c
@@ -1,21 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dveliz-c <dveliz-c@student.42madrid>       +#+  +:+       +#+        */
+/*   By: dveliz-c <dveliz-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/17 19:28:41 by dveliz-c          #+#    #+#             */
-/*   Updated: 2024/09/17 19:29:34 by dveliz-c         ###   ########.fr       */
+/*   Created: 2024/09/17 18:42:15 by dveliz-c          #+#    #+#             */
+/*   Updated: 2024/09/17 19:44:05 by dveliz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-include "libft.h";
-
-int ft_isalnum(int c) {
-    if(ft_isalpha(c) || ft_isdigit(c)){
-        return (1);
-    }
-    return(0);
+	void	*memset(void *s, int c, size_t n){
+		unsigned char *str = s;
+		size_t v;
+		v = 0;
+	while (v < n){
+		*str = (unsigned char)c;
+	}
+	*str ++;
+	return (s);
 }
