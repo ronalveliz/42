a93d57b52cb5f40b30e09e9fcd08fdfc91d54213
RONAL VELIZ <148784629+ronalveliz@users.noreply.github.com>
2024-10-01 00:29:14 +0200

    2 funciones mas

---
+++ PROC/funciones/ft_strchr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/30 20:13:27 by marvin            #+#    #+#             */
+/*   Updated: 2024/09/30 20:13:27 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+//#include <stdio.h> encontrar el primer caracter
+char *ft_strchr(const char *s, int c)
+{
+    while (*s != '\0')
+    {
+        if (*s == (char)c)
+            return (char *)s;
+        s++;
+    }
+    if (c == '\0')
+        return (char *)s;
+    return NULL;
+}
+
+/*int main()
+{
+    char *s = "Hello, World!";
+    char *result = ft_strchr(s, 'o');
+    printf("Resultado: %s\n", result);
+    return 0;
+}
+*/
+
+
---
+++ PROC/funciones/ft_strncmp.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 00:18:13 by marvin            #+#    #+#             */
+/*   Updated: 2024/10/01 00:18:13 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+int ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+    while (n && (*s1 || *s2))
+    {
+        if (*s1 != *s2)
+            return (unsigned char)*s1 - (unsigned char)*s2;
+        s1++;
+        s2++;
+        n--;
+    }
+    return 0;
+}
+int main()
+{
+    char *s1 = "Hello";
+    char *s2 = "Hella";
+    int result = ft_strncmp(s1, s2, 4);
+    printf("Resultado: %d\n", result);
+    return 0;
+}
